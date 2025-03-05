f7972b5a25c030fc56df2900c79074062f61c647
candecarena <carenacandela@gmail.com>
2025-03-05 17:58:47 -0300

    encarpeté mejor las imagenes y llene de info

--- js/archivo.js
+++ js/archivo.js
@@ -9,3 +9,22 @@ function openFullImg(reference){
 function closeImg(){
     fullImgBox.style.display = "none";
 }
+//popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// para el texto al lado de la imagen
+// Seleccionar la imagen y el texto
+const image = document.getElementById("clickableImage");
+const text = document.getElementById("text");
+
+// Agregar un evento de clic a la imagen
+image.addEventListener("click", () => {
+  // Alternar la visibilidad del texto
+  if (text.style.display === "none" || text.style.display === "") {
+    text.style.display = "block"; // Mostrar el texto
+  } else {
+    text.style.display = "none"; // Ocultar el texto
+  }
+});
+
